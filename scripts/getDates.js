fdcaeedefe820e0c3d97ade1b44d380d9e2ce3d6
johnsonomonu <johnsonomonu144@gmail.com>
2024-01-25 03:16:45 +0100

    commit

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -7,3 +7,13 @@ const lastModifiedDate = document.lastModified;
 document.getElementById(
   "lastModified"
 ).textContent = `Last Modified: ${lastModifiedDate}`;
+
+
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
