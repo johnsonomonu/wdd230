2f852b1fda7fcf1dae320b59125e814084e91195
johnsonomonu <johnsonomonu144@gmail.com>
2024-02-14 21:21:51 +0100

    commit

---
+++ data/scripts/links.js
@@ -0,0 +1,41 @@
+const baseURL = "https://johnsonomonu.github.io/wdd230/";
+const linksURL = "https://johnsonomonu.github.io/wdd230/data/links.json";
+
+
+async function getLinks() {
+  try {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+  } catch (error) {
+    console.error("Error fetching links data:", error);
+  }
+}
+
+getLinks();
+
+
+function displayLinks(weeks) {
+  const linksContainer = document.getElementById("activity-links");
+
+  weeks.forEach((week) => {
+    const weekHeader = document.createElement("h2");
+    weekHeader.textContent = week.week;
+    linksContainer.appendChild(weekHeader);
+
+    const linksList = document.createElement("ul");
+
+    week.links.forEach((link) => {
+      const listItem = document.createElement("li");
+      const anchor = document.createElement("a");
+      anchor.href = baseURL + link.url;
+      anchor.textContent = link.title;
+      listItem.appendChild(anchor);
+      linksList.appendChild(listItem);
+    });
+
+    linksContainer.appendChild(linksList);
+  });
+}
+
+
---
+++ week05/scripts/weather.js
@@ -0,0 +1,38 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+const apiKey = "2944340db1fb5d54954628ec3a9b354f"; // Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
+
+const latitude = "49.75";
+const longitude = "6.64";
+const units = "imperial";
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=${units}&appid=${apiKey}`;
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // testing only
+      displayResults(data); // uncomment when ready
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  let desc = data.weather[0].description;
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = `${desc}`;
+}
