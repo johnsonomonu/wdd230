81468c4f9eb955bc4f2ad8060f083bb9b6ebc093
johnsonomonu <johnsonomonu144@gmail.com>
2024-02-23 06:03:19 +0100

    commit

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -165,3 +165,38 @@ function initializeWebsite() {
 
 // Call initializeWebsite() when the page is fully loaded
 window.onload = initializeWebsite;
+
+
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+  hamButton.classList.toggle("open");
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleDarkModeButton = document.getElementById("toggleDarkMode");
+  const body = document.body;
+
+  // Check if user has a dark mode preference stored
+  const darkMode = localStorage.getItem("darkMode") === "enabled";
+
+  // Set initial theme based on user's preference
+  if (darkMode) {
+    body.classList.add("dark-mode");
+  }
+
+  // Toggle dark mode when the button is clicked
+  toggleDarkModeButton.addEventListener("click", function () {
+    body.classList.toggle("dark-mode");
+    const isDarkMode = body.classList.contains("dark-mode");
+
+    // Save user preference to localStorage
+    localStorage.setItem("darkMode", isDarkMode ? "enabled" : "disabled");
+  });
+});
+
+
